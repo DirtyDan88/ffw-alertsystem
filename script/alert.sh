0e0be167cf567eda68ce8f3c87fb9cecb849b072
DirtyDan88 <max.stark88@googlemail.com>
2015-01-24 16:22:07 +0100

    created a test script for several pocsag messages

--- script/alert.sh
+++ script/alert.sh
@@ -1,4 +1,5 @@
 echo "starting chrome..."
-#chromium --app=file:///home/max/workspace/eclipse-java/ffw-alertsystem/$1 
+locationOfScript=$(dirname "$(readlink -e "$0")")
 
-chromium-browser --kiosk file:///home/max/workspace/eclipse-java/ffw-alertsystem/$1 
+# start chrome in fullscreen mode with --kiosk
+chromium-browser --kiosk file://$locationOfScript/../$1 
--- script/test.sh
+++ script/test.sh
@@ -5,16 +5,16 @@ case "$1" in
         echo "Testing watchdog message ..."
         MESSAGE="POCSAG1200: Address:  174896  Function: 0  Alpha:   <BS><CAN>"
         ;;
-    alert)
+    alert1)
         echo "Testing alert message ..."
         MESSAGE="POCSAG1200: Address:  158973  Function: 0  Alpha:   11288/bert/Opek/Magen-Darm-Zentrum M/Bismarckplatz 1/MA-Schwetzingerstadt/WB 3 Ida Scipio Heim/Murgstr. 4/MA-Neckarstadt/12, (laut Pflege geht der Bruder zum Bürgermeister sollte es nicht klappen)/"
         ;;
-    alertWithGeo)
+    alert2)
         echo "Testing alert message with geo coordinates ..."
         MESSAGE="POCSAG1200: Address:  158973  Function: 0  Alpha:   49.324291/08.808761/26/F1 undefiniertes Kleinfeuer//vorm Kindergarten/Industriestraße /Meckesheim// //brennende Mülleimer/"
         ;;
     --help)
-        echo "Usage: $0 {watchdog | alert | alertWithGeo | <message>}"
+        echo "Usage: $0 {watchdog | alert1 | alert2 | <message>}"
         exit 1 
         ;;
     *)
