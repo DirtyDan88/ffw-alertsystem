3cdc74c4b2f1578d2b7d34c30a7550fa18117c71
DirtyDan88 <pi@raspberrypi.(none)>
2015-02-06 13:14:02 +0100

    fixed intall/compile error added run.sh

--- ffw-alertmonitor/install.sh
+++ ffw-alertmonitor/install.sh
@@ -16,9 +16,9 @@ fi
 echo ">> compiling source files ..."
 javac -g:none \
       -d bin/ \
-      -cp lib/* \
-      src/ffw/alertmonitor/util/*.java \
-      src/ffw/alertmonitor/*.java
+      -cp "lib/*" \
+      "src/ffw/alertmonitor/util/*.java \
+      src/ffw/alertmonitor/*.java"
 
 echo ">> let ant do the building work ..."
 ant 
