7f3de21253e5638aebd8073a3e7d862e17de373b
DirtyDan88 <pi@raspi-alertmonitor.(none)>
2015-02-26 20:36:59 +0100

    improved run scripts

--- alertmonitor/run-alertmonitor.sh
+++ alertmonitor/run-alertmonitor.sh
@@ -18,27 +18,29 @@ export DISPLAY=":0"
 # start the alertmonitor
 case "$1" in
     -bg)
+        LOCATION=$(dirname "$(readlink -e "$0")")
+	LOCKFILE=$LOCATION"/.alertmonitor.lock"
+        JARFILE=$LOCATION"/ffw-alertmonitor.jar"
         case "$2" in
             start)
-                if [ -e ".alertmonitor.lock" ] ; then
-#                if [ -n "$(netstat -a | grep $ALERTMONITOR_PORT)" ] ; then
+                if [ -e $LOCKFILE ] ; then
                     echo "alertmonitor is already running"
                 else
                     # run app in background; input and output = null and log into file
-                    nohup java $JAVA_OPT -jar ffw-alertmonitor.jar -logInFile < /dev/null >> /dev/null &
+	             java $JAVA_OPT -jar $JARFILE -logInFile < /dev/null >> /dev/null &
                     # write process id to lock-file
                     PROCESSID="PROCESS-ID: "$!
-                    echo $PROCESSID > ".alertmonitor.lock"
+                    echo $PROCESSID > $LOCKFILE
                 fi
                 ;;
             stop)
                 # get the process id from the lock-file
-                for PID in $(cat .alertmonitor.lock | grep PROCESS-ID) ; do
-                    PROCESSID=$PID
-                done
+                for PID in $(cat $LOCKFILE | grep PROCESS-ID) ; do
+	            PROCESSID=$PID
+        	done
                 echo "kill alertmonitor process: "$PROCESSID
-                kill $PROCESSID
-                rm .alertmonitor.lock
+	        kill $PROCESSID
+                rm $LOCKFILE
                 ;;
             *)
                 echo "Usage: $0 {-bg start | -bg stop}"
--- alertmonitor/run-watchdog.sh
+++ alertmonitor/run-watchdog.sh
@@ -5,27 +5,28 @@ WATCHDOG_PORT=50001
 # start the watchdog
 case "$1" in
     -bg)
+        LOCKFILE=$(dirname "$(readlink -e "$0")")"/.watchdog.lock"
+
         case "$2" in
             start)
-                if [ -n "$(netstat -a | grep $WATCHDOG_PORT)" ] ; then
+                if [ -e $LOCKFILE ] ; then
                     echo "watchdog is already running"
                 else
                     # run app in background; output to log file
                     nohup java -jar ffw-alertsystem-watchdog.jar -logInFile >> /dev/null &
                     # write process id to lock-file
                     PROCESSID="PROCESS-ID: "$!
-                    echo $PROCESSID > ".watchdog.lock"
+                    echo $PROCESSID > $LOCKFILE
                 fi
                 ;;
             stop)
                 # get the process id from the lock-file
-                for PID in $(cat .watchdog.lock | grep PROCESS-ID) ; do
+                for PID in $(cat $LOCKFILE | grep PROCESS-ID) ; do
                     PROCESSID=$PID
                 done
                 echo "kill watchdog process: "$PROCESSID
                 kill $PROCESSID
-                rm .watchdog.lock
-                
+                rm $LOCKFILE
                 ;;
             *)
                 echo "Usage: $0 {-bg start | -bg stop}"
