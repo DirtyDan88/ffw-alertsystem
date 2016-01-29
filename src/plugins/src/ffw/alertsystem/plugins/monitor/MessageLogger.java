075c0cf1be16f52b9ea1925d503aec6187459b28
DirtyDan88 <max.stark88@web.de>
2016-01-30 00:02:31 +0100

    implemented listener plugin

---
+++ src/plugins/src/ffw/alertsystem/plugins/monitor/MessageLogger.java
@@ -0,0 +1,58 @@
+/*
+  Copyright (c) 2015-2016, Max Stark <max.stark88@web.de>
+    All rights reserved.
+  
+  This file is part of ffw-alertsystem, which is free software: you
+  can redistribute it and/or modify it under the terms of the GNU
+  General Public License as published by the Free Software Foundation,
+  either version 2 of the License, or (at your option) any later
+  version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, see <http://www.gnu.org/licenses/>.
+*/
+
+package ffw.alertsystem.plugins.monitor;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
+import ffw.alertsystem.core.message.Message;
+import ffw.alertsystem.core.monitor.MonitorPlugin;
+import ffw.alertsystem.util.DateAndTime;
+
+
+
+public class MessageLogger extends MonitorPlugin {
+  
+  @Override
+  protected void onReceivedMessage(Message message) {
+    File logFileDir = new File(config().paramList().get("log-dir") + 
+                               DateAndTime.getYearAndMonthName());
+    logFileDir.mkdirs();
+    
+    String fileName = "log-" + DateAndTime.getDate() + "-" +
+                      config().paramList().get("log-file-name") + ".txt";
+    File logFile = new File(logFileDir, fileName);
+    
+    FileOutputStream fos = null;
+    try {
+      fos = new FileOutputStream(logFile, true);
+    } catch (FileNotFoundException e) {
+      log.error("could not get log-file for message-logger", e);
+    }
+    
+    PrintStream s = new PrintStream(fos);
+    s.println("[" + DateAndTime.get() + "] " + message.getMessageString());
+    
+    log.info("wrote message to file");
+  }
+  
+}
